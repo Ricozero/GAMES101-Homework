5bdfe8af3c04eb89c4fe268b9103f5580a73cda2
Ricozero <735784247@qq.com>
2024-01-11 17:35:37 +0800

    fix blinn-phong diffuse

--- FinalProject/src/shaders/common.vs
+++ FinalProject/src/shaders/common.vs
@@ -1,35 +1,36 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aNormal;
-out vec3 viewPos;
+out vec3 vertexPos;
 out vec3 vertexNormal;
+out vec3 eyePos;
 
 uniform int screenWidth;
 uniform int screenHeight;
 uniform mat4 view;
 uniform float scale;
 
-// In OpenGL, camera space is left-handed, z+ pointing from camera towards front
-float aspectRatio = float(screenWidth) / screenHeight;
-float t = 400, b = -t, r = t * aspectRatio, l = -r, n = -r, f = r;
-mat4 orth = mat4(
-    2/(r-l), 0, 0, -(r+l)/(r-l),
-    0, 2/(t-b), 0, -(t+b)/(t-b),
-    0, 0, -2/(f-n), -(f+n)/(f-n),
-    0, 0, 0, 1
-);
-
-mat4 model = mat4(
-    scale, 0, 0, 0,
-    0, scale, 0, 0,
-    0, 0, scale, 0,
-    0, 0, 0, 1
-);
-
 void main()
 {
-    vec4 view_pos = view * model * vec4(aPos.x, aPos.y, aPos.z, 1.0);
-    gl_Position = orth * view_pos;
-    viewPos = view_pos.xyz;
+    // In OpenGL, camera space is left-handed, z+ pointing from camera towards front
+    float aspectRatio = float(screenWidth) / screenHeight;
+    float t = 400, b = -t, r = t * aspectRatio, l = -r, n = -r, f = r;
+    mat4 orth = mat4(
+        2/(r-l), 0, 0, -(r+l)/(r-l),
+        0, 2/(t-b), 0, -(t+b)/(t-b),
+        0, 0, -2/(f-n), -(f+n)/(f-n),
+        0, 0, 0, 1
+    );
+
+    mat4 model = mat4(
+        scale, 0, 0, 0,
+        0, scale, 0, 0,
+        0, 0, scale, 0,
+        0, 0, 0, 1
+    );
+
+    gl_Position = orth * view * model * vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    vertexPos = aPos.xyz;
     vertexNormal = aNormal;
+    eyePos = (view * model * vec4(0, 0, t, 1)).xyz;
 }
--- FinalProject/src/shaders/light.fs
+++ FinalProject/src/shaders/light.fs
@@ -1,39 +1,40 @@
 #version 330 core
 in vec3 vertexPos;
 in vec3 vertexNormal;
+in vec3 eyePos;
 out vec4 fragColor;
+
+const vec3 ka = vec3(0.005, 0.005, 0.005);
+const vec3 kd = vec3(0.5f, 1.0f, 0.5f);
+const vec3 ks = vec3(0.7937, 0.7937, 0.7937);
+const vec3 amb_light_intensity = vec3(10, 10, 10);
+const float p = 150;
+
+struct light
+{
+    vec3 position;
+    vec3 intensity;
+};
+const light lights[2] = light[](
+    light(vec3(300, 300, 0), vec3(100000, 100000, 100000)),
+    light(vec3(-300, 300, 0), vec3(100000, 100, 100))
+);
 
 void main()
 {
-    const vec3 ka = vec3(0.005, 0.005, 0.005);
-    const vec3 kd = vec3(0.0f, 1.0f, 0.0f);
-    const vec3 ks = vec3(0.7937, 0.7937, 0.7937);
-
-    struct light
-    {
-        vec3 position;
-        vec3 intensity;
-    };
-    const light lights[1] = light[](
-        light(vec3(20, 20, 20), vec3(500, 500, 500))
-        // light(vec3(-20, 20, 0), vec3(500, 500, 500))
-    );
-
-    const vec3 amb_light_intensity = vec3(10, 10, 10);
-    const vec3 eye_pos = vec3(0, 0, 400);
-    // TODO: 实际上传入的vertexPos是0，为什么这样才能显示正常
+    vec3 eye_pos = eyePos;
     vec3 view_pos = vertexPos;
     vec3 normal = vertexNormal;
-
-    const float p = 150;
-
     vec3 color = vec3(0, 0, 0);
-    for (int i = 0; i < 1; ++i)
+    for (int i = 0; i < 2; ++i)
     {
         vec3 La = ka * amb_light_intensity;
         float r = length(lights[i].position - view_pos);
-        vec3 Ld = kd * lights[i].intensity / (r * r) * max(0.0f, dot(normal, normalize(lights[i].position - view_pos)));
-        vec3 Ls = ks * lights[i].intensity / (r * r) * pow(max(0.0f, dot(normal, normalize((lights[i].position - view_pos + eye_pos - view_pos) / 2))), p);
+        float ndotl = dot(normal, normalize(lights[i].position - view_pos));
+        // vec3 Ld = kd * lights[i].intensity / (r * r) * (sin(ndotl * 3.14159 / 2) + 1);
+        vec3 Ld = kd * lights[i].intensity / (r * r) * (ndotl + 1) / 2;
+        float ndoth = dot(normal, normalize((lights[i].position - view_pos + eye_pos - view_pos) / 2));
+        vec3 Ls = ks * lights[i].intensity / (r * r) * pow(max(0.0f, ndoth), p);
         color += La + Ld + Ls;
     }
     fragColor = vec4(color.x, color.y, color.z, 1.0f);
